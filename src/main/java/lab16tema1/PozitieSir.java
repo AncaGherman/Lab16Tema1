85a7a0a91a5ba1c78bd482e6a26d62507f66df32
Anca G <anca.gherman2013@gmail.com>
2023-02-21 16:57:57 +0200

    Varianta 2

--- src/main/java/lab16tema1/PozitieSir.java
+++ src/main/java/lab16tema1/PozitieSir.java
@@ -1,29 +1,51 @@
 package lab16tema1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PozitieSir implements Runnable {
+    static List<Integer> listPoz = new ArrayList<>();
+    static int nrCautat = 23;
+    static int MAXLENGTH = 11;
+    static int[] sir = {11,23,4,7,2,23,4,45,11,4,23};
+
     private int[] sirInt;
-//    private int pozInc;
-//    private int pozSf;
-    private int nrCautat;
-    //private int MAXLENGTH = 11;
+   // private int nrCautat;
+    private int iDecal;
 
 
-    public PozitieSir(int[] sirInt, int nrCautat) {
+    public PozitieSir(int[] sirInt, int iDecal) {
         this.sirInt = sirInt;
-        this.nrCautat = nrCautat;
+        this.iDecal = iDecal;
     }
 
     public int[] getSirInt() {
         return sirInt;
     }
 
-    public int getNrCautat() {
-        return nrCautat;
+    public int getiDecal() {
+        return iDecal;
     }
 
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(sir));
+
+        int[] sir1 = new int[MAXLENGTH/2];
+        int[] sir2 = new int[MAXLENGTH-MAXLENGTH/2];
+
+        System.arraycopy(sir, 0, sir1, 0,MAXLENGTH/2);
+        System.arraycopy(sir, MAXLENGTH/2, sir2, 0,MAXLENGTH-MAXLENGTH/2);
+
+        PozitieSir sirC1 = new PozitieSir(sir1,0);
+        PozitieSir sirC2 = new PozitieSir(sir2,MAXLENGTH/2);
+
+        sirC1.gasestePoz();
+        sirC2.gasestePoz();
+
+        System.out.println("Numarul " + nrCautat + " este pe pozitiile " + listPoz);
+    }
     public void gasestePoz() {
         Thread t = new Thread(this);
         t.start();
@@ -31,13 +53,12 @@ public class PozitieSir implements Runnable {
     }
 
     public void run() {
-        int lungSirPoz = 0;
         List<Integer> listNrPoz = new ArrayList<>();
-        for (int i=0; i<this.getSirInt().length; i++) {
-            if (sirInt[i] == this.nrCautat) {
-                listNrPoz.add(i);
+        for (int i=0; i<sirInt.length; i++) {
+            if (sirInt[i] == nrCautat) {
+                listPoz.add(i+iDecal);
             }
         }
-        System.out.println(listNrPoz);
+        System.out.println("Acesta este un thread");
     }
 }
--- src/main/java/lab16tema1/PozitieSirPP.java
+++ src/main/java/lab16tema1/PozitieSirPP.java
@@ -1,32 +1,32 @@
-package lab16tema1;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public class PozitieSirPP {
-    public static void main(String[] args) {
-        int MAXLENGTH = 11;
-        int[] sir = {11,23,4,7,2,23,4,45,11,4,23};
-        int nrCautat = 4;
-
-        System.out.println(Arrays.toString(sir));
-        System.out.println("Numarul de cautat: " + nrCautat);
-
-        int[] sir1 = new int[MAXLENGTH/2];
-        int[] sir2 = new int[MAXLENGTH-MAXLENGTH/2];
-
-        //List listNrPoz = new ArrayList<>();
-
-        System.arraycopy(sir, 0, sir1, 0,MAXLENGTH/2);
-        System.arraycopy(sir, MAXLENGTH/2, sir2, 0,MAXLENGTH-MAXLENGTH/2);
-
-        PozitieSir sirC1 = new PozitieSir(sir1, nrCautat);
-        PozitieSir sirC2 = new PozitieSir(sir2, nrCautat);
-
-        sirC1.gasestePoz();
-        sirC2.gasestePoz();
-
-        //System.out.println("Numarul " + nrCautat + " este pe pozitiile " + listNrPoz);
-    }
-}
+//package lab16tema1;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//
+//public class PozitieSirPP {
+//    public static void main(String[] args) {
+//        int MAXLENGTH = 11;
+//        int[] sir = {11,23,4,7,2,23,4,45,11,4,23};
+//        int nrCautat = 4;
+//
+//        System.out.println(Arrays.toString(sir));
+//        System.out.println("Numarul de cautat: " + nrCautat);
+//
+//        int[] sir1 = new int[MAXLENGTH/2];
+//        int[] sir2 = new int[MAXLENGTH-MAXLENGTH/2];
+//
+//        //List listNrPoz = new ArrayList<>();
+//
+//        System.arraycopy(sir, 0, sir1, 0,MAXLENGTH/2);
+//        System.arraycopy(sir, MAXLENGTH/2, sir2, 0,MAXLENGTH-MAXLENGTH/2);
+//
+//        PozitieSir sirC1 = new PozitieSir(sir1, nrCautat);
+//        PozitieSir sirC2 = new PozitieSir(sir2, nrCautat);
+//
+//        sirC1.gasestePoz();
+//        sirC2.gasestePoz();
+//
+//        //System.out.println("Numarul " + nrCautat + " este pe pozitiile " + listNrPoz);
+//    }
+//}
